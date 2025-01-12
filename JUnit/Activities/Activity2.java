3d662e2c3458ee8ca0b05d2f5e6307d860bb6893
Nandhini1620 <nandhini.r7@ibm.com>
2025-01-12 23:12:48 +0530

    Add files via upload

---
+++ JUnit/Activities/Activity2.java
@@ -0,0 +1,33 @@
+package fst_m1_junitNew;
+import org.junit.jupiter.api.Test;
+import activityPrograms.BankAccount;
+import activityPrograms.NotEnoughFundsException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Activity2 {
+	
+	
+
+	class ExpectedExceptionTest {
+
+	    @Test
+	    void notEnoughFunds() {
+	        
+	        BankAccount account = new BankAccount(9);
+
+	       
+	        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10),
+	                "Balance must be greater than amount of withdrawal");
+	    }
+
+	    @Test
+	    void enoughFunds() {
+	        
+	        BankAccount account = new BankAccount(100);
+	    
+	        
+	        assertDoesNotThrow(() -> account.withdraw(100));
+	    }
+	}
+
+}
